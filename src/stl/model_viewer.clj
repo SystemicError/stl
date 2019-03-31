0af78ce15ff5c54c5e75e1c1063cff908c9cd966
Tevis Tsai <SystemicError@gmail.com>
2019-03-31 15:37:57 -0400

    Added primitive z-buffering, now displays triangles.

--- src/stl/model_viewer.clj
+++ src/stl/model_viewer.clj
@@ -7,24 +7,27 @@
   (if (> (count triangles) 0)
     (let [t (first triangles)
           x0 (first (first t))
-          y0 (last (first t))
+          y0 (nth (first t) 1)
           x1 (first (nth t 1))
-          y1 (last (nth t 1))
+          y1 (nth (nth t 1) 1)
           x2 (first (last t))
-          y2 (last (last t))]
+          y2 (nth (last t) 1)
+	  dummy (println (str "\nt:" (into [] t)))]
       (q/with-translation [(/ (q/width) 2.0) (/ (q/height) 2.0)]
+	(q/fill 255 255 255)
         (q/triangle x0 y0 x1 y1 x2 y2))
       (recur (rest triangles)))))
 
 (defn project-point [point camera]
-  "Projects a 3d point to 2d."
+  "Projects a 3d point to 2d, includes z-buffer as third element."
   (let [translated (map + point (:translation camera))
-        rotated (for [row (:rotation camera)] (apply + (map * row translated)))
-        dummy (println (str "point" (into [] point)
-                            "\ntr" (into [] translated)
-                            "\nrot" (into [] rotated)))]
+        rotated (for [row (:rotation camera)] (apply + (map * row translated)))]
+      ;  dummy (println (str "point" (into [] point)
+      ;                      "\ntr" (into [] translated)
+      ;                      "\nrot" (into [] rotated)))]
     (if (< 0.0 (last rotated))
-      (map #(/ % (last rotated)) (take 2 rotated)))))
+      {:point (map #(/ (* (camera :focal-length) %) (last rotated)) (take 2 rotated))
+       :depth (last rotated)})))
 
 (defn project-triangles [triangles camera]
   "Takes in an array of 3d triangles, returns array of 2d triangles."
@@ -32,21 +35,26 @@
 
 (defn draw-state [state]
   "Draws the reflections onto xy plane of triangles receiving light from z-infinitiy."
-  (let [triangles (:triangles state)]
+  (let [triangles (:triangles state)
+        projected (filter #(not= nil %) (project-triangles triangles state))
+        z-buffered (sort #(< (:depth (first %1)) (:depth (first %2))) projected)]
     (q/background 0)
-    (draw-2d-triangles (filter #(not= nil %) (project-triangles triangles state)))
+    ;(println (str (into [] z-buffered)))
+    (draw-2d-triangles (for [t z-buffered] (for [p t] (:point p))))
     ))
 
 (defn setup []
   {:rotation [[1.0 0.0 0.0] [0.0 1.0 0.0] [0.0 0.0 1.0]]
-   :translation [0.0 1.0 19.0]
-   :focal-length 1.0
-   :triangles (for [i (range 10)] (for [j (range 3)] (for [k (range 3)] (* (rand) 100))))
+   :translation [0.0 1.0 1.0]
+   :focal-length 100.0
+   :triangles (for [i (range 16)] (for [j (range 3)] [(- (* (rand) 1000) 500) (- (* (rand) 1000) 500) (* (rand) 1000)]))
+   :time 0.0
    }
   )
 
 (defn update-state [state]
-  (assoc state :translation (map + [0.0 0.0 0.1] (:translation state)))
+  (assoc state :translation (map + [(Math/cos (:time state)) (Math/sin (:time state)) 0.0] (:translation state))
+               :time (+ (:time state) 0.01))
   )
 
 (q/defsketch reflection
